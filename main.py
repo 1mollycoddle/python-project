1bd63b9b7dd752e1654a5a55aa6e0df5cae33d7d
Dmitriy Buslov <godok03@mail.ru>
2022-07-01 21:49:21 +0300

    набросок

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("Привет")
-for i in range(4):
-    print(i+1)
+import re
+import datetime
+import time
+
+
+
+print("О чем мне вам напомнить?")
+message=str(input())
+
+date=re.findall(r"\bзавтра\b",message)
+
+task=re.findall("ть" or "ть" or "чь", message)
+
+if date!=0:
+    print(date, task)
