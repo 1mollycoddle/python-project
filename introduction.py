feeae4b21530bba1afc3b765dd14c021f8d167e2
Dmitriy Buslov <godok03@mail.ru>
2022-07-03 00:02:15 +0300

    1st video without Django

---
+++ introduction.py
@@ -0,0 +1,84 @@
+
+
+#1 task
+print('Hello, World!')
+
+#2 task
+print("value1","value2")
+
+#3 task
+name = 'Dima'
+print(name)
+
+#4 task
+name = input('Whats is your name?')
+print('Hello,', name)
+
+#5 task
+x = float(input('First number:'))
+y = float(input('Second nubmer:'))
+operation = input('Operation:')
+
+result = None
+
+if operation == '+':
+    result = x + y
+elif operation == '-':
+    result = x - y
+elif operation == '*':
+    result = x * y
+elif operation == '/':
+    result = x / y
+else:
+    print('Unsupported operation')
+if result is not None:
+    print('Result:' , result)
+
+#task 6
+from flask import Flask, request, render_template
+
+# Создаём объект веб-приложения
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+
+    return 'Hello World!'
+
+
+@app.route('/hello/', methods=['GET', 'POST'])
+def hello():
+    """
+    По адресу http://адрес.сайта/hello/ у нас будет находится страница,
+    на которой пользователь сможет ввести своё имя и получить приветствие.
+
+    Здесь мы указываем не только URL-адрес, но и разрешённые HTTP-методы.
+    Таким образом, по этому адресу можно будет и получать страницу,
+    и отправлять данные формы, в которой пользователь вводит своё имя
+    """
+
+    if request.method == 'POST':     # если пользователь ввёл своё имя
+        name = request.form['name']  # то получаем его
+    else:
+        name = None                  # иначе присвоим name значение None
+    # После этого отдаём пользователю HTML-страницу на основе шаблона
+    # hello_form.html, в который мы передаём имя пользователя
+    # (HTML можно изучить на соответсвующем курсе по фронтенд-разработке).
+    return render_template('hello_form.html', name=name)
+
+
+@app.route('/<name>')
+def hello_buddy(name):
+    """
+    Любой другой адрес мы считаем именем пользователя
+    и выдаём соответствующее приветствие.
+    """
+    return 'Hello, ' + name + '!'
+
+
+# Если данный файл был запущен как главный, а не импортирован
+# из другого модуля, то запускаем сервер веб-приложения.
+# По умолчанию он будет доступен по адресу http://localhost:5000/
+if __name__ == '__main__':
+    app.run()
