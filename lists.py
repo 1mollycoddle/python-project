72fb255bafe26c6e8edfb1bb3a9c8c31e53b559d
Dmitriy Buslov <godok03@mail.ru>
2022-07-04 14:27:36 +0300

    3 video (not full)

---
+++ lists.py
@@ -0,0 +1,65 @@
+#list_defnition
+print()
+print("list_defnition")
+int_list = [1, 2, 3, 5]
+char_list = ['a', 'c', 'z', 'x']
+empty_list = []
+
+print('Numbers:', int_list)
+print('Chars:', char_list)
+print('Empty lisy:', empty_list)
+
+list_from_range = list(range(5))
+print("From range:", list_from_range)
+list_from_string = list("a string")
+print("From string:" , list_from_string)
+
+#list_indexing
+print()
+print("list_indexing")
+
+my_list = [5, 7, 9, 1, 1, 2]
+
+print(my_list[0])
+print(my_list[1])
+
+index = int(input('Enter an index: '))
+element = my_list[index]
+print(element)
+
+#negative_indices
+print()
+print("negative_indices")
+
+my_list = [5, 7, 9, 1, 1, 2]
+
+pre_last = my_list[-2]
+print(pre_last)
+
+result = my_list[0] + my_list[-1]
+print(result)
+
+#slicing1
+print()
+print('slicing1')
+my_list = [5, 7, 9, 1, 1, 2]
+
+sub_list = my_list[0:3]
+print(sub_list)
+
+print(my_list[2:-2])
+
+print(my_list[4:5])
+
+#slicing2
+print()
+print('slicing2')
+
+
+my_list = [5, 7, 9, 1, 1, 2]
+
+sub_list = my_list[0:-1:2]
+print(sub_list)
+
+print(my_list[2:-2:2])
+print(my_list[-1:0:-1])
