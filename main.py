141ed98fb1eed645a0c40d382052fc316e176609
Dmitriy Buslov <godok03@mail.ru>
2022-07-12 18:46:35 +0300

    work work work

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import re
 import datetime
-import time
 
 
+
+format = "%d.%m.%Y"
 print("О чем мне вам напомнить?")
-message=str(input())
-
-date=re.findall(r"\bзавтра\b",message)
-
-task=(re.search(r'\w+(ть|чь)', message).group())
-
-if date!=0:
-    print(date,task)
+message = input()
+text1 = re.findall(r"[^0-9.,-]", message)
+text2 = ''.join(text1)
+day = re.findall('завтра|понедельник|вторник|среду|четверг|пятницу|субботу|воскресенье', message)
+day1 = ''.join(day)
+datex = re.findall('[0-9][0-9][.,-][0-9][0-9][.,-][0-9][0-9][0-9][0-9 ]', message)
+date_string = ''.join(datex)
+dt2 = datetime.datetime.strptime(date_string, format)
+print (text2)
+print(date_string)
+print(datetime.date.today())
+print(dt2)
